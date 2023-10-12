aab52e62bac176cbd41b0464a3a4161c3a086475
Sergio <sergioariaaas@gmail.com>
2023-10-12 09:06:54 -0300

    Update preentrega1.js
    Segunda Pre entrega

--- preentrega1.js
+++ preentrega1.js
@@ -6,29 +6,71 @@ function edadpermitida(edad) {
     let nombre = prompt("Ingrese su nombre");
     let apellido = prompt("Ingrese su apellido");
     alert("Bienvenido/a, " + nombre + " " + apellido + "!");
-    
-    let productos = ["short", "remera", "medias"];
-    alert("Productos disponibles: short, remera, medias");
-    
-    let operacion = prompt("Ingrese el nombre del producto que desea:");
-    
-    switch (operacion) {
-      case "short":
-        alert("El producto elegido es short y su precio es de $5.000");
+    const productos = [
+  {nombre: "Camiseta", precio: 8000 },
+  {nombre: "Short", precio: 5000 },
+  {nombre: "Buzo", precio: 10000},
+  {nombre: "Joggin", precio: 6500},
+];
+
+let carrito = [];
+
+let seleccion = prompt("Desea comprar algun producto? si/no")
+
+while(seleccion != "si" && seleccion !="no"){
+  alert("Por favor, ingrese si o no")
+  seleccion = prompt ("Hola, desea comprar algun producto? si/no")
+}
+if (seleccion == "si"){
+  alert("A continuacion visualizara la lista de productos")
+  let todoslosproductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$")
+  alert(todoslosproductos.join(" - "))
+} else if (seleccion == "no"){
+  alert("Muchas gracias por visitarnos, hasta pronto")
+}
+
+while(seleccion != "no"){
+  let producto = prompt("Agrega un producto a tu carrito (Camiseta, Short, Buzo, Joggin)")
+  let precio = 0
+
+  if (producto == "Camiseta" || producto == "Short" || producto == "Buzo" || producto == "Joggin"){
+    switch (producto){
+      case "Camiseta":
+        precio = 8000;
         break;
-      case "remera":
-        alert("El producto elegido es remera y su precio es de $10.000");
+      case "Short":
+        precio = 5000;
         break;
-      case "medias":
-        alert("El producto elegido es medias y su precio es de $3.000");
+      case "Buzo":
+        precio = 10000;
         break;
+      case "Joggin":
+        precio = 6500;
+        break; 
       default:
-        alert("El producto ingresado no se encuentra");
         break;
     }
-  } else {
+    let unidades = parseInt(prompt("Cuantas unidades quiere llevar?"))
+    carrito.push({producto, unidades, precio})
+
+  }else{
+    alert("No tenemos ese producto 😒")
+  }
+  seleccion = prompt("Desea comprar otro producto? si/no")
+  while(seleccion === "no"){
+    alert("Gracias por su compra")
+    carrito.forEach((carritoFinal) =>{
+      alert(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar: ${carritoFinal.unidades * carritoFinal.precio}`)
+    })
+    break;
+  }
+}
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
+alert("El total a pagar es " + total)
+    
+} else {
     alert("Necesita ser mayor de edad (+18)");
   }
 }
 
-edadpermitida(edad); 
+edadpermitida(edad);
